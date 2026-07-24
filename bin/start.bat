d750cf1162c1391f77f034bce3fe3d80807241e0
dpt-hashimoto <ysn_hashimoto@dpt-inc.co.jp>
2026-07-24 09:36:04 +0900

    start.bat修正

--- bin/start.bat
+++ bin/start.bat
@@ -2,7 +2,7 @@
 
 cd /d %~dp0
 
-powershell -NoProfile -Command "$p = Start-Process 'runtime\bin\javaw.exe' -ArgumentList '-jar ..\copilot-alart-1.0.0.jar' -PassThru; $p.Id > 'pid.txt'"
+powershell -NoProfile -Command "$p = Start-Process '..\runtime\bin\javaw.exe' -ArgumentList '-jar ..\copilot-alart-1.0.0.jar' -PassThru; $p.Id > 'pid.txt'"
 
 
 pause
