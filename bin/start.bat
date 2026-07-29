1d3ad2a6c34d21cc3f32722976e9a63943e70482
dpt-hashimoto <ysn_hashimoto@dpt-inc.co.jp>
2026-07-29 14:28:26 +0900

    Jenkinsの成果物で動作しなかったのでデバッグ用に修正

--- bin/start.bat
+++ bin/start.bat
@@ -3,8 +3,6 @@
 REM 製品ディレクトリへ移動
 cd /d "%~dp0.."
 
-powershell -NoProfile -Command ^
-"$p = Start-Process 'runtime\bin\javaw.exe' -ArgumentList '-jar copilot-alart-1.0.0.jar' -PassThru; $p.Id > 'bin\pid.txt'"
-
+runtime\bin\java.exe -jar copilot-alart-1.0.0.jar
 
 pause
--- src/main/java/com/example/demo/CopilotAlart.java
+++ src/main/java/com/example/demo/CopilotAlart.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CopilotAlart {
 
 	public static void main(String[] args) {
+	    System.out.println("Current Directory = " + System.getProperty("user.dir"));
 		SpringApplication.run(CopilotAlart.class, args);
 	}
 
